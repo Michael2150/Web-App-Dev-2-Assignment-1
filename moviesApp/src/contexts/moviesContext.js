d257d3edffba4a0d362673af4642125ed55a9941
Michael2150 <michaelgerb2150@gmail.com>
2022-11-15 14:06:08 +0000

    Fork from labsl repo to form a base to work off of

---
+++ moviesApp/src/contexts/moviesContext.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [myReviews, setMyReviews] = useState( {} ) 
+  const [favourites, setFavourites] = useState( [] )
+  const [mustWatch, setMustWatch] = useState( [] )
+
+  const addToFavourites = (movie) => {
+    let newFavourites = [...favourites];
+    if (!favourites.includes(movie.id)) {
+      newFavourites.push(movie.id);
+    }
+    setFavourites(newFavourites);
+  };
+
+  // We will use this function in a later section
+  const removeFromFavourites = (movie) => {
+    setFavourites( favourites.filter(
+      (mId) => mId !== movie.id
+    ) )
+  };
+
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+
+  const addToMustWatch = (movie) => {
+    let newMustWatch = [...mustWatch];
+    if (!mustWatch.includes(movie.id)) {
+      newMustWatch.push(movie.id);
+    }
+    setMustWatch(newMustWatch);
+  };
+
+  // We will use this function in a later section
+  const removeFromMustWatch = (movie) => {
+    setMustWatch( mustWatch.filter(
+      (mId) => mId !== movie.id
+    ) )
+  };
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favourites,
+        addToFavourites,
+        removeFromFavourites,
+        addReview,
+        mustWatch,
+        addToMustWatch,
+        removeFromMustWatch,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
