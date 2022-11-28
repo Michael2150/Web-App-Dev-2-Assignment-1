af42fe7963a8a5041823185b5ca081c24e14cd88
Michael2150 <michaelgerb2150@gmail.com>
2022-11-28 00:06:35 +0000

    Start of implementing authentication

---
+++ moviesApp/src/contexts/authContext.js
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+
+
+const AuthContext = createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(auth.currentUser) 
+    const [loading, setLoading] = useState(true)
+
+    function createUser(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout() {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+    }, [])
+
+    const value = { 
+        currentUser,
+        createUser,
+        login,
+        logout
+    }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
