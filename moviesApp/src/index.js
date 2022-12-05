9e7560bbf837e0b6fa3a6ca8d8046f5480dcdf53
Michael2150 <michaelgerb2150@gmail.com>
2022-12-05 14:15:23 +0000

    Added the endpoints

--- moviesApp/src/index.js
+++ moviesApp/src/index.js
@@ -3,20 +3,28 @@ import SiteHeader from './components/siteHeader'
 import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movies/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/movies/favouriteMoviesPage"; 
-import MovieReviewPage from "./pages/movies/movieReviewPage";
-import UpcomingMoviesPage from "./pages/movies/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
-import AddMovieReviewPage from './pages/movies/addMovieReviewPage';
 import LoginPage from "./pages/loginPage";
-import MoviesListPage from "./pages/movies/moviesListPage";
 import PrivateRoute from './privateRoute'
 import Contexts from "./contexts/contextWrapper";
 import SignupPage from "./pages/signupPage";
 import AccountPage from "./pages/accountDetailsPage";
 import LogoutPage from "./pages/logoutPage";
+
+import AddMovieReviewPage from './pages/movies/addMovieReviewPage';
+import MoviePage from "./pages/movies/movieDetailsPage";
+import FavouriteMoviesPage from "./pages/movies/favouriteMoviesPage"; 
+import MovieReviewPage from "./pages/movies/movieReviewPage";
+import UpcomingMoviesPage from "./pages/movies/upcomingMoviesPage";
+import MoviesListPage from "./pages/movies/moviesListPage";
+
+import AddShowReviewPage from './pages/shows/addShowReviewPage';
+import ShowPage from "./pages/shows/showDetailsPage";
+import FavouriteShowsPage from "./pages/shows/favouriteShowsPage";
+import ShowReviewPage from "./pages/shows/showReviewPage";
+import UpcomingShowsPage from "./pages/shows/upcomingShowsPage";
+import ShowsListPage from "./pages/shows/showsListPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -50,6 +58,16 @@ const App = () => {
             <Route path="/movies/upcoming/:page" element={<PrivateRoute> <UpcomingMoviesPage/> </PrivateRoute>} />
             <Route path="movies/reviews/form" element={<PrivateRoute> <AddMovieReviewPage/> </PrivateRoute>} />
             <Route path="movies/reviews/:id" element={<PrivateRoute> <MovieReviewPage/> </PrivateRoute>} />
+
+            {/* Shows Section */}
+            <Route path="/shows" element={<PrivateRoute> <Navigate to="/shows/1" /> </PrivateRoute>} />
+            <Route path="/shows/:page" element={<PrivateRoute> <ShowsListPage/> </PrivateRoute>} />
+            <Route path="/show/:id" element={<PrivateRoute> <ShowPage/> </PrivateRoute>} />
+            <Route exact path="/shows/favourites" element={<PrivateRoute> <FavouriteShowsPage/> </PrivateRoute>} />
+            <Route path="/shows/upcoming" element={<PrivateRoute> <Navigate to="shows/upcoming/1" /> </PrivateRoute>} />
+            <Route path="/shows/upcoming/:page" element={<PrivateRoute> <UpcomingShowsPage/> </PrivateRoute>} />
+            <Route path="shows/reviews/form" element={<PrivateRoute> <AddShowReviewPage/> </PrivateRoute>} />
+            <Route path="shows/reviews/:id" element={<PrivateRoute> <ShowReviewPage/> </PrivateRoute>} />
 
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
